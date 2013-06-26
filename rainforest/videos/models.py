ccbff34d7dd137bcb30309067f23d6d65d302db7
Neil Kimmett <neilkimmett@gmail.com>
2013-06-27 00:39:29 +0100

    Fix admin, add migration to fix table name

--- rainforest/videos/models.py
+++ rainforest/videos/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-from django.contrib.auth import User
+from django.contrib.auth.models import User
 
-class Videos(models.Model):
+class Video(models.Model):
     name = models.CharField(max_length=140)
     user = models.ForeignKey(User)
